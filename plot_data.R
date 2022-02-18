23871d25349a0f5ff70e5fae4a4ed4dd0628ac26
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-02-18 10:47:48 -0500

    Update plot function

--- plot_data.R
+++ plot_data.R
@@ -9,29 +9,48 @@ list2env(lapply(setNames(files, make.names(sub("*.csv$", "", basename(files)))),
          envir = .GlobalEnv)
 
 # plot function
-plot_prov <- function(data, type = c("daily", "cumulative"), title) {
+plot_value <- function(data, type = c("daily", "cumulative"), title, hr = FALSE) {
   match.arg(type, choices = c("daily", "cumulative"), several.ok = FALSE)
   if (type == "daily") {y <- "value_daily"} else {y <- "value"}
-  ggplot(data = data, aes(x = date, y = !!sym(y), colour = province, group = province)) +
-    geom_line() +
-    theme_pubclean() +
-    labs(title = title) +
-    theme(plot.title = element_text(hjust = 0.5))
+  if (hr) {
+    ggplot(data = data, aes(x = date, y = !!sym(y), colour = province,
+                            group = paste(province, sub_region_1))) +
+      geom_line(alpha = 0.3) +
+      theme_pubclean() +
+      labs(title = title) +
+      theme(plot.title = element_text(hjust = 0.5)) +
+      guides(colour = guide_legend(override.aes = list(alpha = 1)))
+  } else {
+    ggplot(data = data, aes(x = date, y = !!sym(y), colour = province, group = province)) +
+      geom_line() +
+      theme_pubclean() +
+      labs(title = title) +
+      theme(plot.title = element_text(hjust = 0.5))
+  }
 }
 
 # generate plots
 
 ## cases
-plot_prov(cases_prov, type = "daily", title = "Daily case data")
+plot_value(cases_prov, type = "daily", title = "Daily case data")
+ggsave("plots/cases_prov.png", width = 731, height = 603, units = "px", dpi = 96)
+plot_value(cases_hr, type = "daily", title = "Daily case data (health regions)", hr = TRUE)
+ggsave("plots/cases_hr.png", width = 731, height = 603, units = "px", dpi = 96)
 
 ## mortality
-plot_prov(mortality_prov, type = "daily", title = "Daily mortality data")
+plot_value(mortality_prov, type = "daily", title = "Daily mortality data")
+ggsave("plots/mortality_prov.png", width = 731, height = 603, units = "px", dpi = 96)
+plot_value(mortality_hr, type = "daily", title = "Daily mortality data (health regions)", hr = TRUE)
+ggsave("plots/mortality_hr.png", width = 731, height = 603, units = "px", dpi = 96)
 
 ## testing
-plot_prov(tests_completed_prov, type = "daily", title = "Daily tests completed")
+plot_value(tests_completed_prov, type = "daily", title = "Daily tests completed")
+ggsave("plots/tests_completed_prov.png", width = 731, height = 603, units = "px", dpi = 96)
 
 ## hospitalization
-plot_prov(hosp_prov, type = "cumulative", title = "Hospitalization data")
+plot_value(hosp_prov, type = "cumulative", title = "Hospitalization data")
+ggsave("plots/hosp_prov.png", width = 731, height = 603, units = "px", dpi = 96)
 
 ## ICU
-plot_prov(icu_prov, type = "cumulative", title = "ICU data")
+plot_value(icu_prov, type = "cumulative", title = "ICU data")
+ggsave("plots/icu_prov.png", width = 731, height = 603, units = "px", dpi = 96)
