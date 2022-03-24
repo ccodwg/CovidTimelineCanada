861918df33af44e21ba74e16a5cc81b46de80958
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-03-24 10:39:42 -0400

    Plot function: add option to hide negative values

--- plot_data.R
+++ plot_data.R
@@ -9,11 +9,11 @@ list2env(lapply(setNames(files, make.names(sub("*.csv$", "", basename(files)))),
          envir = .GlobalEnv)
 
 # plot function
-plot_value <- function(data, type = c("daily", "cumulative"), title, hr = FALSE) {
+plot_value <- function(data, type = c("daily", "cumulative"), title, hr = FALSE, hide_negative_values = FALSE) {
   match.arg(type, choices = c("daily", "cumulative"), several.ok = FALSE)
   if (type == "daily") {y <- "value_daily"} else {y <- "value"}
   if (hr) {
-    ggplot(data = data, aes(x = date, y = !!sym(y), colour = province,
+    p <- ggplot(data = data, aes(x = date, y = !!sym(y), colour = province,
                             group = paste(province, sub_region_1))) +
       geom_line(alpha = 0.3) +
       theme_pubclean() +
@@ -21,26 +21,32 @@ plot_value <- function(data, type = c("daily", "cumulative"), title, hr = FALSE)
       theme(plot.title = element_text(hjust = 0.5)) +
       guides(colour = guide_legend(override.aes = list(alpha = 1)))
   } else {
-    ggplot(data = data, aes(x = date, y = !!sym(y), colour = province, group = province)) +
+    p <- ggplot(data = data, aes(x = date, y = !!sym(y), colour = province, group = province)) +
       geom_line() +
       theme_pubclean() +
       labs(title = title) +
       theme(plot.title = element_text(hjust = 0.5))
   }
+  # hide negative values
+  if (hide_negative_values) {
+    p <- p + ylim(0, NA)
+  }
+  # return plot
+  p
 }
 
 # generate plots
 
 ## cases
-plot_value(cases_prov, type = "daily", title = "Daily case data")
+plot_value(cases_prov, type = "daily", title = "Daily case data", hide_negative_values = TRUE)
 ggsave("plots/cases_prov.png", width = 731, height = 603, units = "px", dpi = 96)
-plot_value(cases_hr, type = "daily", title = "Daily case data (health regions)", hr = TRUE)
+plot_value(cases_hr, type = "daily", title = "Daily case data (health regions)", hr = TRUE, hide_negative_values = TRUE)
 ggsave("plots/cases_hr.png", width = 731, height = 603, units = "px", dpi = 96)
 
 ## mortality
-plot_value(mortality_prov, type = "daily", title = "Daily mortality data")
+plot_value(mortality_prov, type = "daily", title = "Daily mortality data", hide_negative_values = TRUE)
 ggsave("plots/mortality_prov.png", width = 731, height = 603, units = "px", dpi = 96)
-plot_value(mortality_hr, type = "daily", title = "Daily mortality data (health regions)", hr = TRUE)
+plot_value(mortality_hr, type = "daily", title = "Daily mortality data (health regions)", hr = TRUE, hide_negative_values = TRUE)
 ggsave("plots/mortality_hr.png", width = 731, height = 603, units = "px", dpi = 96)
 
 ## testing
