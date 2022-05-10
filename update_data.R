6c0225e9320a0f2477df401e88f1ee255dc0411c
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-05-09 22:21:48 -0400

    Add GitHub update to update_data.R

--- update_data.R
+++ update_data.R
@@ -7,6 +7,8 @@
 # Authentication: You must authenticate your Google account before running the rest of the script.
 # You may be asked to give "Tidyverse API Packages" read/write access to your Google account.
 
+# GitHub: This script assumes pre-existing authentication for pushing to the CovidTimelineCanada GitHub repository.
+
 # run update
 if (file.exists("/secrets.json")) {
   # use service account key, if it exists
@@ -17,4 +19,41 @@ if (file.exists("/secrets.json")) {
 } else {
   # otherwise, prompt for authentication
   Covid19CanadaETL::ccodwg_update()
+}
+
+# check for updated files
+status <- system2("git", c("diff-index", "--quiet HEAD"))
+if (status == 0) {
+  # exit without update
+  cat("No files have changed. Exiting without update...", fill = TRUE)
+} else {
+  # generate update time
+  update_time <- Covid19CanadaETL::update_time()
+  # stage data update
+  cat("Staging files for update...", fill = TRUE)
+  system2(
+    command = "git",
+    args = c("add",
+             "data/",
+             "raw_data/",
+             "update_time.txt"
+    )
+  )
+  # commit data update
+  cat("Creating commit...", fill = TRUE)
+  system2(
+    command = "git",
+    args = c("commit",
+             "-m",
+             paste0('"', 'Update data: ', update_time, '"')
+    )
+  )
+  # push data update
+  cat("Pushing data update...", fill = TRUE)
+  system2(
+    command = "git",
+    args = c("push")
+  )
+  # report success
+  cat("Data update successful!", fill = TRUE)
 }
