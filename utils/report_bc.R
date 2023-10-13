d090e3f97a41c1bb02d0eb81ba4ea77bf427984b
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2023-10-13 00:33:49 -0400

    Update BC report script (#95)
    - Include cumulative time series for cases, deaths, hosp_admissions, icu_admissions (#111, #118)

--- utils/report_bc.R
+++ utils/report_bc.R
@@ -122,11 +122,123 @@ Covid19CanadaData::webdriver_close(remDr)
 dat_testing <- dplyr::bind_rows(lapply(dat_testing, function(z) data.frame(x = z$x, y = z$y))) |>
   dplyr::transmute(
     date = date_local,
-    source = "https://bccdc.shinyapps.io/respiratory_covid_sitrep/#Test_rates_and_percent_positivity",
+    source = "",
     date_start = lubridate::date(as.POSIXct(x / 1000, origin = "1970-01-01", tz = "UTC")),
     date_end = date_start + 6,
     region = "BC",
-    value = y)
+    sub_region_1 = "",
+    tests_completed = y)
+
+# add source for first entry
+dat_testing[1, "source"] <- "https://bccdc.shinyapps.io/respiratory_covid_sitrep/#Test_rates_and_percent_positivity"
 
 # sync data to Google Sheets
 googlesheets4::write_sheet(data = dat_testing, ss = "1ZTUb3fVzi6CLZAbU3lj6T6FTzl5Aq-arBNL49ru3VLo", sheet = "bc_monthly_report_testing")
+
+### CUMULATIVE CASES, DEATHS, HOSPITAL ADMISSIONS, ICU ADMISSIONS BY HEALTH AUTHORITY ###
+
+# open dashboard with webdriver
+remDr <- Covid19CanadaData::webdriver_open("https://bccdc.shinyapps.io/respiratory_covid_sitrep/#Test_rates_and_percent_positivity")
+Sys.sleep(15) # wait 15 seconds for dashboard to load
+
+# change tab from "Weekly totals" to "Historical totals"
+remDr$findElement(using = "css selector", "a[data-value='Historical totals']")$clickElement()
+
+# wait for tab to update
+Sys.sleep(3)
+
+# function: extract cumulative table
+extract_cum_tab <- function(date_current) {
+  # get table
+  tab <- remDr$findElement(using = "css selector", "#table2 table")$getElementAttribute("outerHTML")[[1]] |>
+    rvest::read_html() |>
+    rvest::html_table(header = FALSE) |>
+    (\(x) x[[1]])()
+  # create and process data frame
+  tab_names <- c("name", "notes", as.character(tab[2, 3:9]))
+  tab_names <- dplyr::case_when(
+    tab_names == "FH" ~ "Fraser Health",
+    tab_names == "IH" ~ "Interior Health",
+    tab_names == "NH" ~ "Northern Health",
+    tab_names == "VCH" ~ "Vancouver Coastal Health",
+    tab_names == "VIHA" ~ "Island Health",
+    tab_names == "OOC" ~ "Out of Canada",
+    TRUE ~ tab_names)
+  names(tab) <- tab_names
+  tab <- tab[3:7, -2]
+  tab[, "name"] <- c("hosp_admissions", "icu_admissions", "deaths_up_to_2022-03-31", "deaths_after_2022-04-01", "cases")
+  tab <- tab
+  tab <- data.frame(t(tab))
+  names(tab) <- as.character(tab[1, 1:5])
+  col_1 <- row.names(tab)[2:8]
+  col_1[col_1 == "Total"] <- ""
+  tab <- tab[-1, ]
+  tab <- tab |>
+    dplyr::mutate(dplyr::across(dplyr::everything(), function(x) readr::parse_number(gsub(",", "", x))))
+  tab |>
+    dplyr::transmute(
+      date = date_local,
+      source = "",
+      date_start = date_start,
+      date_end = date_current,
+      region = "BC",
+      sub_region_1 = col_1,
+      .data$cases,
+      .data$`deaths_up_to_2022-03-31`,
+      .data$`deaths_after_2022-04-01`,
+      .data$hosp_admissions,
+      .data$icu_admissions) |>
+    dplyr::arrange(.data$sub_region_1)
+}
+
+# function: update "to" value for date slider
+# http://ionden.com/a/plugins/ion.rangeslider/demo_interactions.html
+update_date <- function(date_current) {
+  d <- as.numeric(as.POSIXct(date_current, tz = "UTC")) * 1000
+  remDr$executeScript(sprintf("var $d = $('#DatesMerge').data('ionRangeSlider'); $d.update({to: %s});", d))
+}
+
+# function: check if table div has class "recalculating"
+check_recalculating <- function() {
+  div_classes <- remDr$findElement(using = "css selector", "#table2")$getElementAttribute("class")[[1]]
+  "recalculating" %in% strsplit(div_classes, " ")
+}
+
+# get date range
+date_start <- remDr$findElement(using = "css selector", "div[data-value='Historical totals'] span.irs-min")$getElementAttribute("innerHTML")[[1]]
+date_end <- remDr$findElement(using = "css selector", "div[data-value='Historical totals'] span.irs-max")$getElementAttribute("innerHTML")[[1]]
+
+# get epoch values for all dates between date_start and date_end
+dates <- seq.Date(from = as.Date(date_start), to = as.Date(date_end), by = "day")
+
+# initialize list
+dat_cum <- list()
+
+# loop through dates
+for (x in 1:length(dates)) {
+  d <- dates[x]
+  cat(as.character(d), fill = TRUE)
+  # update date slider
+  update_date(d)
+  # wait for table to update
+  Sys.sleep(3)
+  # extract table
+  dat_cum[[as.character(d)]] <- extract_cum_tab(d)
+}
+
+# close webdriver
+Covid19CanadaData::webdriver_close(remDr)
+
+# format data frame
+dat_cum <- dplyr::bind_rows(dat_cum)
+
+# add combined deaths column
+dat_cum <- dat_cum |>
+  dplyr::mutate(
+    deaths = .data$`deaths_up_to_2022-03-31` + .data$`deaths_after_2022-04-01`, .after = .data$`deaths_after_2022-04-01`)
+
+# add source for first entry
+dat_cum[1, "source"] <- "https://bccdc.shinyapps.io/respiratory_covid_sitrep/#COVID-19_cases,_hospitalizations,_critical_care_admissions_and_deaths"
+
+# sync data to Google Sheets
+googlesheets4::write_sheet(data = dat_cum, ss = "1ZTUb3fVzi6CLZAbU3lj6T6FTzl5Aq-arBNL49ru3VLo", sheet = "bc_monthly_report_cumulative")
